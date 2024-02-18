8cb16ad4aee1001610987526be7385d9c13d5ea2
ravikiranreddy <ravikiranreddy1118@gmail.com>
2024-02-19 00:20:06 +0530

    zustand library added

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import PolicyHolderForm from './PolicyHolderForm'; // Import the PolicyHolderForm component
+// import PolicyHolderForm from './PolicyHolderForm'; // Import the PolicyHolderForm component
+import {useStore} from './store'
+import {shallow} from 'zustand/shallow'
 
 function App() {
-  return (
-    <div className="App">
-      <PolicyHolderForm /> {/* Render the PolicyHolderForm component */}
-    </div>
-  );
+  let tasks = useStore(store => store.tasks);
+  tasks = JSON.stringify(tasks)
+  console.log(tasks, "Hi")
+  const addTask = useStore((store) => store.addTask)
+  return <div>
+  {tasks}
+  <br />
+  <button onClick={()=>{addTask("asdsa", "successful")}}>
+    Add Task
+  </button>
+  </div>
 }
-
-export default App;
+  export default App;
